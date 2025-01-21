d380274ad2ea7b8b9ec1dcc59b97fa550209620e
Alain Manzi <munyeshurimanzi@gmail.com>
2025-01-21 11:15:07 +0200

    add design

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 
 import React from 'react';
-import { Col, Input, Row } from 'antd';
+import { Col, Row } from 'antd';
 
 interface Props {
   name: string;
@@ -36,14 +36,14 @@ const CustomInput: React.FC<Props> = ({
         </label>
       </Col>
       <Col xs={{ span: 23 }} lg={{ span: 18 }}>
-        <Input
+        <input
           id={name}
           type={type}
           placeholder={label}
           defaultValue={defaultValue}
           min={min}
           max={max}
-          {...register(name, { required, ...rules })} 
+          {...register(name, { required, ...rules })} // Spread the rules into the register function
           className={`input-field  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors[name] ? 'input-field-error' : ''}`}
         />
         {errors[name] && (
